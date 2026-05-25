40a6043d1fce8c545e6b33f04eaeafb6a9164091
evgenbala7-beep <evgenbala7@gmail.com>
2026-05-25 23:45:01 +0300

    keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from threading import Thread
+app = Flask(__name__)
+@app.route('/')
+def home(): return "Бот работает!"
+def run(): app.run(host='0.0.0.0', port=8080)
+def keep_alive(): Thread(target=run).start()
